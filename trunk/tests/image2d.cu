41afd75ac6b0ff519044c5e832ba7c9495a1d94e
garrigues <garrigues@02274fd6-12ee-40d1-98bc-127062ca8bd6>
2011-02-02 08:43:36 +0000

    [cuimg] Add tests.

---
+++ trunk/tests/image2d.cu
@@ -0,0 +1,14 @@
+
+#include <cuda_runtime.h>
+
+
+//#include <cuimg/improved_builtin.h>
+//#include <cuimg/image2d.h>
+
+//using namespace cuimg;
+
+int main()
+{
+ //image2d<i_float4> img(300, 400);
+//  test<<<42, 54>>>(test);
+}
