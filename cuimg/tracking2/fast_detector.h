29525b499b6a86abe260f6b62630a4fb8539768b
Matthieu Garrigues <matthieu.garrigues@ensta.fr>
2013-03-04 17:44:02 +0100

    Start CUDA fast detector

--- cuimg/tracking2/fast_detector.h
+++ cuimg/tracking2/fast_detector.h
@@ -7,12 +7,17 @@
 namespace cuimg
 {
 
+  template <typename A>
   class fast_detector
   {
   public:
+    typedef typename A::image2d<gl8u>::ret image2d_gl8u;
+    typedef typename A::image2d<i_short2>::ret image2d_short2;
+
     inline fast_detector(const obox2d& d);
 
     inline void update(const host_image2d<gl8u>& input);
+    inline void update(const device_image2d<gl8u>& input);
 
     template <typename F, typename PS>
     inline void new_particles(const F& feature, PS& pset);
@@ -20,17 +25,17 @@ namespace cuimg
     inline fast_detector& set_fast_threshold(float f);
     inline fast_detector& set_n(unsigned n);
 
-    inline const host_image2d<gl8u>& saliency() { return saliency_; }
-    inline const host_image2d<gl8u>& contrast() { return contrast_; }
+    inline const image2d_gl8u& saliency() { return saliency_; }
+    inline const image2d_gl8u& contrast() { return contrast_; }
 
   private:
     float contrast_th_;
     float fast_th_;
     unsigned n_;
-    host_image2d<gl8u> saliency_;
-    host_image2d<gl8u> contrast_;
-    host_image2d<gl8u> input_;
-    host_image2d<char> new_points_;
+    image2d_gl8u saliency_;
+    image2d_gl8u contrast_;
+    image2d_gl8u input_;
+    image2d_short2 new_points_;
   };
 
 }
--- cuimg/tracking2/fast_detector.hpp
+++ cuimg/tracking2/fast_detector.hpp
@@ -6,72 +6,6 @@
 namespace cuimg
 {
 
-  namespace fast
-  {
-
-    template <typename I>
-    inline gl8u compute_saliency(i_short2 p, const I& input_, int scale, int n_, int fast_th_)
-    {
-      unsigned max_n = 0;
-      unsigned n = 0;
-      int status = 2;
-      gl8u vp = input_(p);
-      unsigned sum_bright = 0;
-      unsigned sum_dark = 0;
-
-      for (int i = 0; i < 16; i++)
-      {
-	gl8u vn = input_(p + i_int2(circle_r3_h[i]));
-	int sign = int(vn) > int(vp);
-	unsigned char dist = ::abs(int(vn) - int(vp));
-	if (dist > fast_th_)
-	{
- 	  if (sign == status) n++;
-	  else
-	  {
-	    if (n > max_n)
-	      max_n = n;
-	    status = sign;
-	    n = 1;
-	  }
-
-	  if (vn < vp)
-	    sum_dark += dist;
-	  else
-	    sum_bright += dist;
-	}
-	else
-	  status = 2;
-      }
-
-      if (n != 16 && status != 2)
-      {
-	int i = 0;
-	while (true)
-	{
-	  gl8u vn = input_(p + i_int2(circle_r3_h[i]));
-	  int sign = int(vn) > int(vp);
-	  unsigned char dist = ::abs(int(vn) - int(vp));
-
-	  if (dist <= fast_th_ || sign != status) break;
-
-	  n++;
-	  i++;
-	  assert(i < 16);
-	}
-
-      }
-
-      if (n > max_n)
-	max_n = n;
-
-      if (max_n < n_)
-	return 0;
-      else
-	return std::max(sum_bright, sum_dark);
-    }
-  }
-
   fast_detector::fast_detector(const obox2d& d)
     : n_(9),
       saliency_(d),
@@ -94,6 +28,8 @@ namespace cuimg
     return *this;
   }
 
+
+#ifndef NO_CPP0X
   void
   fast_detector::update(const host_image2d<gl8u>& input)
   {
@@ -102,19 +38,18 @@ namespace cuimg
     mt_apply2d(sizeof(i_float1), input.domain() - border(8),
 	       [this, &input] (i_int2 p)
 	       {
-		 saliency_(p) = fast::compute_saliency(p, input, 1, n_, fast_th_);
+		 saliency_(p) = fast::compute_saliency(p, input, n_, fast_th_);
 	       }, arch::cpu());
 
     // contrast
     mt_apply2d(sizeof(i_float1), input.domain() - border(5),
 	       [this, &input] (i_int2 p)
 	       {
-		 const int d = 5;
-		 contrast_(p) = ::abs(int(input(p + i_int2(0,d))) - int(input(p + i_int2(0,-d)))) +
-		   ::abs(int(input(p + i_int2(-d,0))) - int(input(p + i_int2(d,0))));
+		 contrast_(p) = fast::compute_contrast(p, input, contrast_);
 	       }, arch::cpu());
 
   }
+
 
   template <typename F, typename PS>
   void
@@ -145,6 +80,71 @@ namespace cuimg
                }, arch::cpu());
 
   }
+
+
+#endif
+
+#ifndef NO_CUDA
+
+  template <typename F, typename PS, typename I, typename J>
+  inline __global__
+  void select_particles(PS& pset, const I& saliency, J& new_points)
+  {
+    i_int2 p = thread_pos2d();
+
+    if (pset.has(p)) return;
+    if (saliency_(p) == 0) return;
+
+    for (int i = 0; i < 8; i++)
+    {
+      i_int2 n(p + i_int2(c8[i]));
+      if (saliency_(p) < saliency_(n) || pset.has(n))
+	return;
+    }
+
+    new_points_(p) = p;
+  }
+
+
+  template <typename F, typename PS>
+  void
+  fast_detector::new_particles(const F& feature, PS& pset)
+  {
+    SCOPE_PROF(fast_new_particles_detector);
+    memset(new_points_, 0);
+
+    select_particles<<<dimgrid, dimblock>>>()
+    CUDA_ITERATE()
+    {
+
+    }
+
+    mt_apply2d(sizeof(i_float1), saliency_.domain() - border(8),
+               [this, &feature, &pset] (i_int2 p)
+               {
+                 if (pset.has(p)) return;
+                 if (saliency_(p) == 0) return;
+
+                 for (int i = 0; i < 8; i++)
+                 {
+                   i_int2 n(p + i_int2(c8[i]));
+                   if (saliency_(p) < saliency_(n) || pset.has(n))
+                     return;
+                 }
+
+                 new_points_(p) = 1;
+               }, arch::cpu());
+
+    st_apply2d(sizeof(i_float1), saliency_.domain() - border(8),
+               [this, &feature, &pset] (i_int2 p)
+               {
+                 if (new_points_(p)) pset.add(p, feature(p));
+               }, arch::cpu());
+
+  }
+
+
+#endif
 
 }
 
